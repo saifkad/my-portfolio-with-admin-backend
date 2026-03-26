839c6f783bc52109debef46ea793b7da6686f537
Saif Al-Diresee <saif.aldiresee@gmail.com>
2026-03-26 19:24:21 +0100

    Fix: Update clean code, Add favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Saif Dev Portfolio',
   description: 'Professional developer portfolio',
+  icons: {
+    icon: '/public/logo.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
