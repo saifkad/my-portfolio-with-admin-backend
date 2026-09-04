2089e5e3ed56d90b5e318f68b494a3a316a90ec7
saifkad <saif1990kad@gmail.com>
2026-09-04 18:02:07 +0200

    Hide blog navlink

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -10,7 +10,7 @@ const navItems = [
   { label: 'About', href: '#about' },
   { label: 'Skills', href: '#skills' },
   { label: 'Projects', href: '#projects' },
-  { label: 'Blog', href: '/blog' },
+  // { label: 'Blog', href: '/blog' },
   { label: 'Contact', href: '#contact' },
 ];
 
